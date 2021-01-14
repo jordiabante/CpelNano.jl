705c2725d9ccfa5c3b75603fbe38139b060210b9
jordiabante <jordiabante@protonmail.com>
2021-01-14 19:42:33 +0100

    Minor changes.

--- calls/PaperCalls/Subregion-Analysis.jl
+++ calls/PaperCalls/Subregion-Analysis.jl
@@ -9,7 +9,7 @@ using StatsPlots
 using KernelDensity
 using LinearAlgebra
 using DelimitedFiles
-default(titlefont=(14,"arial"),guidefont=(16,"arial"),tickfont=(12,"arial"))
+default(titlefont=(14,"arial"),guidefont=(16,"arial"),tickfont=(12,"arial"),legendfont=(12,"arial"))
 
 #####################################################################################################
 # Functions
@@ -17,12 +17,12 @@ default(titlefont=(14,"arial"),guidefont=(16,"arial"),tickfont=(12,"arial"))
 function read_in_data(data_dir::String,sub_max_sizes::Vector{Int64})
 
     # Ranges
-    num_cpgs_rng = 0:2:200
-    sub_size_rng = 0:10:600
+    num_cpgs_rng = 0:1:25
+    sub_size_rng = 0:20:600
     sts_sub_size = collect(sub_size_rng)
     sts_num_cpgs = collect(num_cpgs_rng)
-    cent_sub_size = sts_sub_size.+step(sub_size_rng)/2.0
-    cent_num_cpgs = sts_num_cpgs.+step(num_cpgs_rng)/2.0
+    cent_sub_size = sts_sub_size .- step(sub_size_rng)/2.0
+    cent_num_cpgs = sts_num_cpgs .- step(num_cpgs_rng)/2.0
     
     # Read in data
     size_histos = []
@@ -31,7 +31,7 @@ function read_in_data(data_dir::String,sub_max_sizes::Vector{Int64})
     for sub_size in sub_max_sizes
         println("$(sub_size)")
         # Read in data
-        sub_data = readdlm("$(data_dir)/subregion_$(sub_size)_study/subregion_table.txt")[:,3:4]
+        sub_data = readdlm("$(data_dir)/subregion_$(sub_size)_study/subregion_table.txt")
         # Get histograms
         sub_size_histo = fit(Histogram,sub_data[:,1],sub_size_rng)
         num_cpgs_histo = fit(Histogram,sub_data[:,2],num_cpgs_rng)
@@ -62,15 +62,15 @@ sub_max_sizes = [100,150,250,300,350,400,450,500]
 # Plot subregion sizes
 p1 = plot(ylim=(0,100),xlabel="Analysis region size (bp)",ylabel="Percentage (%)")
 for (i,size) in enumerate(sub_max_sizes)
-    plot!(cent_sub_size,size_histos[i].weights*100,seriestype=:bar,alpha=0.5,label="$(size) bp")
+    plot!(cent_sub_size,size_histos[i].weights*100,seriestype=:bar,alpha=0.5,label="")
 end
 
 # Plot number of CpG sites
-p2 = plot(xlim=(0,50),ylim=(0,100),xlabel="Number of CpG sites",ylabel="Percentage (%)")
+p2 = plot(ylim=(0,100),xlabel="Number of CpG sites",ylabel="Percentage (%)",legend=:best,legendtitle="Max Size")
 for (i,size) in enumerate(sub_max_sizes)
     plot!(cent_num_cpgs,cpgs_histos[i].weights*100,seriestype=:bar,alpha=0.5,label="$(size) bp")
 end
 
 # Store plot
-plot(p1,p2,size=(600,600),layout=(2,1))
+plot(p2,p1,size=(600,700),layout=(2,1))
 savefig("$(data_dir)/Subregion-Properties.pdf")
