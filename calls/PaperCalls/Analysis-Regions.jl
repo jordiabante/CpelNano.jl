2e3b0dc9f68d87fb73b58c86e257953ec1472a50
jordiabante <jordiabante@protonmail.com>
2021-01-16 11:09:02 +0100

    Changed plot layout.

--- calls/PaperCalls/Analysis-Regions.jl
+++ calls/PaperCalls/Analysis-Regions.jl
@@ -9,7 +9,7 @@ using StatsPlots
 using KernelDensity
 using LinearAlgebra
 using DelimitedFiles
-default(titlefont=(14,"arial"),guidefont=(16,"arial"),tickfont=(12,"arial"),legendfont=(12,"arial"))
+default(titlefont=(14,"arial"),guidefont=(14,"arial"),tickfont=(10,"arial"),legendfont=(12,"arial"))
 blnd_frnd_col = ["#999999","#E69F00","#56B4E9","#009E73","#F0E442","#0072B2","#D55E00","#CC79A7"]
 
 #####################################################################################################
@@ -54,25 +54,32 @@ end
 
 # Data dir
 base_dir = "/Users/jordiabante/OneDrive - Johns Hopkins"
-data_dir = "$(base_dir)/CpelNano/Data/Real-Data/Subregion-Properties-hg38/"
+data_dir = "$(base_dir)/CpelNano/Data/Real-Data/Analysis-Region-Properties-hg38/"
 
 # Produce histos
-sub_max_sizes = [100,200,300,400,500]
+sub_max_sizes = [100,150,200,250,300,350,400,450,500]
 (cent_sub_size,size_histos),(cent_num_cpgs,cpgs_histos) = read_in_data(data_dir,sub_max_sizes)
 
 # Plot subregion sizes
-p1 = plot(ylim=(0,100),xlabel="Analysis region size (bp)",ylabel="Percentage (%)")
+p1s = []
 for (i,size) in enumerate(sub_max_sizes)
-    plot!(cent_sub_size,size_histos[i].weights*100,seriestype=:bar,alpha=0.5,label="",color=blnd_frnd_col[i+1])
-end
-
-# Plot number of CpG sites
-p2 = plot(ylim=(0,100),xlabel="Number of CpG sites",ylabel="Percentage (%)",legend=:best,legendtitle="Max Size")
-for (i,size) in enumerate(sub_max_sizes)
-    plot!(cent_num_cpgs,cpgs_histos[i].weights*100,seriestype=:bar,alpha=0.5,label="$(size) bp",
-        color=blnd_frnd_col[i+1],xticks=0:2:25)
+    p = plot(cent_sub_size,size_histos[i].weights*100,seriestype=:bar,alpha=0.5,label="",title="$(size) bp",
+        ylim=(0,100),xlabel="Analysis region size (bp)",ylabel="Percentage (%)")
+    push!(p1s,p)
 end
 
 # Store plot
-plot(p2,p1,size=(600,700),layout=(2,1))
-savefig("$(data_dir)/Subregion-Properties.pdf")
+plot(p1s...,size=(1000,1000),layout=(3,3))
+savefig("$(data_dir)/Analysis-Region-Sizes.pdf")
+
+# Plot number of CpG sites
+p2s = []
+for (i,size) in enumerate(sub_max_sizes)
+    p = plot(cent_num_cpgs,cpgs_histos[i].weights*100,seriestype=:bar,alpha=0.5,label="",xticks=0:2:25,
+        ylim=(0,100),xlabel="Number of CpG sites",ylabel="Percentage (%)",title="$(size) bp",tickfont=(10,"arial"))
+    push!(p2s,p)
+end
+
+# Store plot
+plot(p2s...,size=(1000,1000),layout=(3,3))
+savefig("$(data_dir)/Analysis-Region-CpG-Histogram.pdf")
